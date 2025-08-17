b3c46439bfe1b995fb1724c3ce59dd6263292ef1
Martin Dlouhy <md@robotika.cz>
2025-08-17 21:39:55 +0200

    install vnc and audio

--- odroid/install.sh
+++ odroid/install.sh
@@ -1,3 +1,6 @@
+apt install xserver-xorg-video-dummy
+apt install portaudio19-dev
+
 cp 10-headless.conf /etc/X11/xorg.conf.d/10-headless.conf
 cp vnc-display.sh /usr/local/bin/vnc-display.sh
 cp vnc-display.service /etc/systemd/system/vnc-display.service
