6e8a76083912dc71050c1387d99f540abbb2b289
Martin Dlouhy <md@robotika.cz>
2025-08-24 14:05:49 +0200

    install.sh - start sections

--- odroid/install.sh
+++ odroid/install.sh
@@ -1,8 +1,29 @@
-apt install xserver-xorg-video-dummy
-apt install portaudio19-dev
+install_dummy_display() {
+  # and audio
+  apt install xserver-xorg-video-dummy
+  apt install portaudio19-dev
 
-cp 10-headless.conf /etc/X11/xorg.conf.d/10-headless.conf
-cp vnc-display.sh /usr/local/bin/vnc-display.sh
-cp vnc-display.service /etc/systemd/system/vnc-display.service
-systemctl enable vnc-display.service
+  cp 10-headless.conf /etc/X11/xorg.conf.d/10-headless.conf
+  cp vnc-display.sh /usr/local/bin/vnc-display.sh
+  cp vnc-display.service /etc/systemd/system/vnc-display.service
+  systemctl enable vnc-display.service
+}
 
+install_pip() {
+  sudo apt install python3-pip
+}
+
+prepare_git_structure(){
+  mkdir git
+  cd git/
+  mkdir bare
+  cd bare/
+  git init –bare osgar.git
+  git init –bare osgar-apps.git
+  git init –bare matty.git
+  mkdir ~/logs
+}
+
+
+# high level calls
+prepare_git_structure
