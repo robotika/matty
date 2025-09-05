9cc942301fb664e659849068e7182870f0d5ca2a
Martin Dlouhy <md@robotika.cz>
2025-09-05 18:39:35 +0200

    vnc-display.sh - comment out even checking of status

--- odroid/vnc-display.sh
+++ odroid/vnc-display.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HDMI_STATUS=$(cat /sys/class/drm/card?-HDMI-A-1/status)
+#HDMI_STATUS=$(cat /sys/class/drm/card?-HDMI-A-1/status)
 #if [ "$HDMI_STATUS" = "connected" ]; then
 #    sudo mv /etc/X11/xorg.conf.d/10-headless.conf /etc/X11/xorg.conf.d/10-headless.conf.bak
 #else
